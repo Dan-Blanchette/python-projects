67b463a48327435eaf0af18c759c916a88761367
Dan-Blanchette <dlblanchette@students.nic.edu>
2021-05-11 18:56:17 -0700

    added middle name to the method

--- ch-1/PC 1/fullname.py
+++ ch-1/PC 1/fullname.py
@@ -1,5 +1,6 @@
-first_name = "Edsgar"
+first_name = "Edsger"
+middle =  "Wybe"
 last_name = "Dijkstra"
-full_name = f"{first_name} {last_name}" # f"{}" format allows the printing of multiple string values encased in curley brackets.
+full_name = f"{first_name} {middle} {last_name}" # f"{}" format allows the printing of multiple string values encased in curley brackets.
 message = f"Hello, {full_name.title()}!" # adds the title modifier to the full_name variable as well as an additional string message.
 print(message) 
