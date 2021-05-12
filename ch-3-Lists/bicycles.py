bd6e45e61f2f6e7589c7031490294e23adf15e25
Dan-Blanchette <dlblanchette@students.nic.edu>
2021-05-12 13:08:06 -0700

    lists practice part 1

---
+++ ch-3-Lists/bicycles.py
@@ -0,0 +1,6 @@
+bicycles = ['trek', 'cannodale', 'redline', 'specialized'] # to create a list in python the [] are required after variable declaration
+print(bicycles) # prints the list of bikes
+print(bicycles[0]) # print a specific entry from the list. In this case, bicycles[0] = trek
+print(bicycles[0].title()) # formatting modifiers can also be appended to list print outs.
+print(bicycles[1]) # just like an array in C++, the list position being accessed is ( n - 1 ) of the list position you want to print out
+print(bicycles[3])
