af6010224595eb7f426f66612faf1511fb158bfd
Dan-Blanchette <dlblanchette@students.nic.edu>
2021-05-11 19:29:16 -0700

    learning about how integer values work in Python vs other languages

---
+++ ch-1/PC 1/integers.py
@@ -0,0 +1,12 @@
+x,y,z = 0,0,0 # in python you can initialize multiple varibles
+x = x + 1.0
+y = y + 3.5
+z = z + 5 # there is no need to declare data types
+mult = x * y * z # 1.0 * 3.5 * 5 
+div = (z + y) / x
+add = x + y + z
+sub = z - y - x
+print(mult) # all these will be float values for printing as Python auto converts to correct data type
+print(div)
+print(add)
+print(sub)
