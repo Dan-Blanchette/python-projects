d8566dad1a7b4626e7dc256b05b70a979e5f710e
Dan-Blanchette <dlblanchette@students.nic.edu>
2021-05-12 13:32:59 -0700

    added friends.py (programming challenge) updated comments on bicycles.py

--- ch-3-Lists/bicycles.py
+++ ch-3-Lists/bicycles.py
@@ -4,3 +4,6 @@ print(bicycles[0]) # print a specific entry from the list. In this case, bicycle
 print(bicycles[0].title()) # formatting modifiers can also be appended to list print outs.
 print(bicycles[1]) # just like an array in C++, the list position being accessed is ( n - 1 ) of the list position you want to print out
 print(bicycles[3])
+print(bicycles[-1]) # the negative values will print out starting with the last item of the list
+message = f"\n\nMy first bicycle was a {bicycles[0].title()}" # list values can be formatted and printed
+print(message)
