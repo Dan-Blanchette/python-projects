593bc6317c6d2e94ea3666ce9c873092c2c2eb23
Dan-Blanchette <dlblanchette@students.nic.edu>
2021-05-11 19:07:25 -0700

    whitespace practice

---
+++ ch-1/PC 1/whitespace.py
@@ -0,0 +1,9 @@
+#print("Python") # normal printout
+#print("\tPython") # adds a tab to the output
+#print("\nLanguages: \nPython\nC\nC++\nJavascript") # add newline
+#print("\nLanguages: \n\tPython\n\tC\n\tC++\n\tJavascript") # add newline
+message = '                  Python                                                       '
+print(message)
+print(message.strip())
+print(message.rstrip())
+print(message.lstrip())
