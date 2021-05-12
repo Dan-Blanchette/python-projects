1620707c0d87e3a2eb1ac49a761c095dd491394c
Dan-Blanchette <dlblanchette@students.nic.edu>
2021-05-11 18:54:40 -0700

    string practice on Python

---
+++ ch-1/PC 1/fullname.py
@@ -0,0 +1,5 @@
+first_name = "Edsgar"
+last_name = "Dijkstra"
+full_name = f"{first_name} {last_name}" # f"{}" format allows the printing of multiple string values encased in curley brackets.
+message = f"Hello, {full_name.title()}!" # adds the title modifier to the full_name variable as well as an additional string message.
+print(message) 
