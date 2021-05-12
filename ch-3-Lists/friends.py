765c06d55eaffb00a3bf24fd30ad02d5270af211
Dan-Blanchette <dlblanchette@students.nic.edu>
2021-05-12 13:42:30 -0700

    fixed some spelling errors

--- ch-3-Lists/friends.py
+++ ch-3-Lists/friends.py
@@ -3,7 +3,7 @@ friends = ['tim hendershot','corey koerner','john malterer','ryan booth']
 message1 = f"\n\n{friends[0].title()} is my oldest friend that I have known since elementry school."
 message2 = f"{friends[1].title()} is a DnD GM extrordinaire."
 message3 = f"{friends[2].title()} is often times a horse in the games we play, but I forgive him."
-message4 = f"{friends[3].title()} is also known as rocky BalBootha.\n\n"
+message4 = f"{friends[3].title()} is also known as Rocky BalBootha.\n\n"
 print(message1)
 print(message2)
 print(message3)
