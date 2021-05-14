33ed69c26fddbb7d84f0a1ffca063b27421c1552
Dan-Blanchette <dlblanchette@students.nic.edu>
2021-05-14 08:01:34 -0700

    program complete added more comments for clarification

--- automateTBS/ch-1-pyBasics/firstProg.py
+++ automateTBS/ch-1-pyBasics/firstProg.py
@@ -2,10 +2,10 @@
 
 print('Hello World!')
 print('What is your name?') # ask for user's name
-myName = input()
+myName = input() # get users name
 print('It is good to meet you ' + myName)
 print('The length of my name is:')
-print(len(myName))
+print(len(myName)) # prints the number of characters for the string that was entered
 print('What is your age?')
-myAge = input()
-print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+myAge = input() # get an integer number from the user
+print('You will be ' + str(int(myAge) + 1) + ' in a year.') # print user's age plus 1 year
