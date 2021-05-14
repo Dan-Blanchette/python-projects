9a675f96dac85b5e00f212d3b00fa506a078b46f
Dan-Blanchette <dlblanchette@students.nic.edu>
2021-05-14 08:12:25 -0700

    simple logic statement examples

---
+++ automateTBS/ch-1-pyBasics/password.py
@@ -0,0 +1,9 @@
+name = 'Mary'
+password = 'swordfish'
+
+if name == 'Mary':
+    print('Hello ' + name)
+    if password == 'swordfish':
+        print('Access Granted')
+    else:
+        print('Wrong Password.')
