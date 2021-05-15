8c2514a6863c7fe5ac76c18cef9ad7dd0db1e112
Dan-Blanchette <dlblanchette@students.nic.edu>
2021-05-14 20:10:47 -0700

    basic function program

---
+++ automateTBS/ch-3-Functions/funct.py
@@ -0,0 +1,10 @@
+# function definition will print out 3 different messages
+def hello():
+    print('Howdy!')
+    print('Howdy!!')
+    print('Hello There.')
+    
+# Here we call the function 3 times
+hello()
+hello()
+hello()
