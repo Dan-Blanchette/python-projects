062c818e3ddae8c64ba4f3d5aea8ed713be6f73b
Dan-Blanchette <dlblanchette@students.nic.edu>
2021-05-14 20:24:50 -0700

    basic function with passed input

---
+++ automateTBS/ch-3-Functions/def.py
@@ -0,0 +1,10 @@
+# this program is an example of functions that have parameters
+# prompt user
+print('What is your name?')
+# function returns the print out of the name entered
+def hello(name):
+    print('Hello, ' + name)
+# get user input
+name2 = input()
+# call function
+hello(name2)
